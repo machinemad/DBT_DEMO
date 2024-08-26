b25a43f50b6a8fdd52dbf7de8d6de90c8ac52335
Abhijit Bhavsar <abhijit.bhavsar@diacto.com>
2024-08-26 19:20:38 +0000

    Primary Commit

--- models/Order_Details.sql
+++
@@ -1,4 +0,0 @@
--- models/create_order_details_table.sql
-{{ config(materialized='table') }}
-
-{{ create_table_from_stage('@"DBT_PP_DB"."PIZZA_PLACE"."RAW_CSVS"/order_details.csv', 'order_details') }}
